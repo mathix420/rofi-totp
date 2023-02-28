67479d3a13f53d98a0f17bf0f53eda45fc62ecd8
Arnaud Gissinger <agissing@student.42.fr>
2023-02-28 15:56:52 +0100

    [FIX] totp usage

--- src/totp.rs
+++ src/totp.rs
@@ -1,8 +1,11 @@
 extern crate base32;
 extern crate totp_lite;
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
 pub fn generate(secret: &str) -> Result<String, &'static str> {
   let secret_bytes = (base32::decode(base32::Alphabet::RFC4648 {padding: false}, &secret.replace(" ", "")).ok_or("invalid base32"))?;
-  let code: String = totp_lite::totp::<totp_lite::Sha1>(&secret_bytes, 30);
+  let seconds: u64 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+  let code: String = totp_lite::totp_custom::<totp_lite::Sha1>(totp_lite::DEFAULT_STEP, 6, &secret_bytes, seconds);
   Ok(format!("{:0>6}", code))
 }
